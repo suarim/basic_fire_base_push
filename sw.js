b6ddd5de8bfabaf92e37fd8f61bf5dbcb4e54aa4
suarim <muhammadsuarim@gmail.com>
2025-02-07 11:12:36 +0530

    hide vapid key

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: ""*************************",",
+    apiKey: "*************************",
     authDomain: "first-846ad.firebaseapp.com",
     projectId: "first-846ad",
     storageBucket: "first-846ad.firebasestorage.app",
